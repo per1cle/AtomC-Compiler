33b187e3316c535cba231bd347781de0d5d2d058
per1cle <lazartatiana02@gmail.com>
2026-06-11 13:10:23 +0300

    done

--- src/gc.c
+++ src/gc.c
@@ -1,32 +1,47 @@
 #include "gc.h"
 
-void insertConvIfNeeded(Instr *before,Type *srcType,Type *dstType){
-	switch(srcType->tb){
-		case TB_INT:
-			switch(dstType->tb){
-				case TB_DOUBLE:
-					insertInstr(before,OP_CONV_I_F);
-					break;
-				}
-			break;
+void insertConvIfNeeded(Instr *before, Type *srcType, Type *dstType)
+{
+	switch (srcType->tb)
+	{
+	case TB_INT:
+		switch (dstType->tb)
+		{
 		case TB_DOUBLE:
-			switch(dstType->tb){
-				case TB_INT:
-					insertInstr(before,OP_CONV_F_I);
-					break;
-				}
+			insertInstr(before, OP_CONV_I_F);
+			break;
+		default:
 			break;
 		}
+		break;
+	case TB_DOUBLE:
+		switch (dstType->tb)
+		{
+		case TB_INT:
+			insertInstr(before, OP_CONV_F_I);
+			break;
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
 	}
+}
 
-void addRVal(Instr **code,bool lval,Type *type){
-	if(!lval)return;
-	switch(type->tb){
-		case TB_INT:
-			addInstr(code,OP_LOAD_I);
-			break;
-		case TB_DOUBLE:
-			addInstr(code,OP_LOAD_F);
-			break;
-		}
+void addRVal(Instr **code, bool lval, Type *type)
+{
+	if (!lval)
+		return;
+	switch (type->tb)
+	{
+	case TB_INT:
+		addInstr(code, OP_LOAD_I);
+		break;
+	case TB_DOUBLE:
+		addInstr(code, OP_LOAD_F);
+		break;
+	default:
+		break;
 	}
+}
